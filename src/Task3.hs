a38ec1ac80eeaad6c230a39352d4bc062e4740b5
ptntsv <hitogavax@ya.ru>
2026-02-18 23:45:55 +0700

    Task 3

--- src/Task3.hs
+++ src/Task3.hs
@@ -1,13 +1,16 @@
 {-# OPTIONS_GHC -Wall #-}
+
 -- The above pragma enables all warnings
 
 module Task3 where
 
 -- Explicit import of Prelude to hide functions
 -- that are not supposed to be used in this assignment
-import Prelude hiding (compare, foldl, foldr, Ordering(..))
 
-import Task1 (Tree(..))
+import Data.Foldable (Foldable (foldr))
+import Task1 (Order (InOrder), Tree (..), torder)
+import Task2 (Cmp, Ordering (..), tdelete, tinsert, tlookup)
+import Prelude hiding (Ordering (..), compare, foldl, foldr)
 
 -- * Type definitions
 
@@ -15,6 +18,15 @@ import Task1 (Tree(..))
 type Map k v = Tree (k, v)
 
 -- * Function definitions
+
+--
+-- >>> compareKeys  (1, 'e') (3, 'a')
+-- True
+compareKeys :: (Ord k) => Cmp (k, v)
+compareKeys (k1, _) (k2, _)
+  | k1 < k2 = LT
+  | k1 == k2 = EQ
+  | otherwise = GT
 
 -- | Construction of 'Map' from association list
 --
@@ -24,9 +36,8 @@ type Map k v = Tree (k, v)
 -- Branch (2,'a') (Branch (1,'b') Leaf Leaf) (Branch (3,'c') Leaf Leaf)
 -- >>> listToMap [] :: Map Int Char
 -- Leaf
---
-listToMap :: Ord k => [(k, v)] -> Map k v
-listToMap = error "TODO: define listToMap"
+listToMap :: (Ord k) => [(k, v)] -> Map k v
+listToMap = foldr (uncurry minsert) Leaf
 
 -- | Conversion from 'Map' to association list sorted by key
 --
@@ -36,9 +47,8 @@ listToMap = error "TODO: define listToMap"
 -- [(1,'b'),(2,'a'),(3,'c')]
 -- >>> mapToList Leaf
 -- []
---
 mapToList :: Map k v -> [(k, v)]
-mapToList = error "TODO: define mapToList"
+mapToList = torder InOrder Nothing
 
 -- | Searches given 'Map' for a value associated with given key
 --
@@ -51,9 +61,10 @@ mapToList = error "TODO: define mapToList"
 -- Just 'b'
 -- >>> mlookup 'a' Leaf
 -- Nothing
---
-mlookup :: Ord k => k -> Map k v -> Maybe v
-mlookup = error "TODO: define mlookup"
+mlookup :: (Ord k) => k -> Map k v -> Maybe v
+mlookup x map' = case tlookup compareKeys (x, undefined) map' of
+  Just (_, val) -> Just val
+  Nothing -> Nothing
 
 -- | Inserts given key and value into given 'Map'
 --
@@ -68,9 +79,8 @@ mlookup = error "TODO: define mlookup"
 -- Branch (2,'a') (Branch (1,'X') Leaf Leaf) (Branch (3,'c') Leaf Leaf)
 -- >>> minsert 1 'X' Leaf
 -- Branch (1,'X') Leaf Leaf
---
-minsert :: Ord k => k -> v -> Map k v -> Map k v
-minsert = error "TODO: define minsert"
+minsert :: (Ord k) => k -> v -> Map k v -> Map k v
+minsert k v = tinsert compareKeys (k, v)
 
 -- | Deletes given key from given 'Map'
 --
@@ -83,6 +93,5 @@ minsert = error "TODO: define minsert"
 -- Branch (2,'a') Leaf (Branch (3,'c') Leaf Leaf)
 -- >>> mdelete 'a' Leaf
 -- Leaf
---
-mdelete :: Ord k => k -> Map k v -> Map k v
-mdelete = error "TODO: define mdelete"
+mdelete :: (Ord k) => k -> Map k v -> Map k v
+mdelete k' = tdelete compareKeys (k', undefined)
